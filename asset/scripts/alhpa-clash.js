b544e0020c90c127365f44f4f7786c122ac6a244
Nafiul Miraj <nafiulmiraj@gmail.com>
2024-05-10 08:42:27 +0600

    js basics done

---
+++ asset/scripts/alhpa-clash.js
@@ -0,0 +1,16 @@
+// function play()
+// {
+//     // 1 hide home
+//     const homeSection = document.getElementById('home-screen');
+// homeSection.classList.add('hidden');
+//     // show playground 
+
+//     const playgroundShow = document.getElementById('play-ground');
+// playgroundShow.classList.remove('hidden');
+// }
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+
+}
