e85e693b5096dc437dbfe45c2d535640d41f8cc4
Nafiul Miraj <nafiulmiraj@gmail.com>
2024-06-08 15:36:13 +0600

    clean

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,16 +1,3 @@
-// function play(){
-//     // step 1 ; hide home screen, to hide the add screen ,class hidden  to the home section
-//     const homeSection= document.getElementById('home-screen');
-//     homeSection.classList.add('hidden');
-
-
-//     // show the playground
-
-//     const playgroundSection=document.getElementById('play-ground');
-//     playgroundSection.classList.remove('hidden');}
-
-
-
 
 function handleKeyboardKeyUpEvent(event){
 const playerPressed=event.key;
@@ -58,18 +45,6 @@ else {
     }  
 }
 
-
- 
-//     console.log('player pressed', playerPressed);
-
-// get the expected to press
-// const currentAlphabetElement=document.getElementById('current-alphabet');
-// const currentAlphabet=currentAlphabetElement.innerText;
-// const expectedAlphabet=currentAlphabet.toLowerCase();
-// console.log(playerPressed, expectedAlphabet);
-
-
-// console.log(currentAlphabetElement.innerText);
 
 
 
